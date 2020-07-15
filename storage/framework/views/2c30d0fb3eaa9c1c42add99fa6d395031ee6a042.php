ed64b1ac01c555be0357c161467018469d36c87b
xtrememib01 <xtrememib01@gmail.com>
2020-07-15 18:25:38 +0530

    1st

---
+++ storage/framework/views/2c30d0fb3eaa9c1c42add99fa6d395031ee6a042.php
@@ -0,0 +1,45 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('inc.messages', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="container">
+        <div class="page-header text-center mt-4">
+            <h1>Calendar view</h1>
+        </div>
+        
+
+        
+        
+        <div class="container">
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100px180/" alt="">
+                <div class="card-body ">
+                    <a href="/bookroom/create">
+                        <div class="row">
+                            <i class="fa fa-address-book fa-3x col-1 mr-0 pr-0"></i>
+                            <h4 class="card-title col-11  mt-2 ml-0 pl-0 ">Book  a conference room</h4>
+                        </div>
+                        <p class="card-text ml-5">Click on the above icon to book a conference room</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-4 ml-3 mr-3">
+            <div id='calendar'></div>
+        </div>
+        <div id='timer.js'></div>
+
+        
+    
+            
+        
+
+
+                    
+                </tbody>
+            </table>
+            </div>
+        
+    </div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/durgesh/Documents/Laravel/Projects/MeetingRoomOriginal/resources/views/bookroom/index.blade.php ENDPATH**/ ?>
